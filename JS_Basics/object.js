ab691f8fcdf79f20e2fc3051201dedd04c27b3cc
PrarthiVarshney31 <129403570+PrarthiVarshney31@users.noreply.github.com>
2023-11-10 18:55:59 +0000

    Day5

--- JS_Basics/object.js
+++ JS_Basics/object.js
@@ -37,3 +37,57 @@ userdata.greeting2=function () {
     console.log(`Hello User,${this.name}`);
 }
 console.log(userdata.greeting2());
+
+// Singleton Object
+const tinder1=new Object();
+console.log(tinder1);
+
+//Non Singleton Object
+const tinder2={};
+
+const user={
+    id:12345,
+    email:"abc.com",
+    age:23,
+    fullname:{
+        first:"Prarthi",
+        Last:"Varshney"
+    }
+}
+console.log(user.fullname);     //object of first,Last
+console.log(user.fullname.first); //Prarthi
+// console.log(user.fullname?.first);  --> Agr fullname exist krta hoga toh wrna if else krwaenge
+
+// Combining Objects
+
+const obj1={1:"a",2:"b"};
+const obj2={3:"c",4:"d"};
+// const ob3={obj1,obj2};   object of two objects will be form
+
+// To form object from elements of 2 or more objects
+// const ob3=Object.assign({},obj1,obj2)  
+// object obj3 will contain all elements of obj1,obj2
+
+//  const obj3=Object.assign({},obj1,obj2,obj4)
+// {}-->target
+//obj1,2,4-->source
+
+
+//  To Combine Oject Using Spread
+const obj3={...obj1,...obj2}
+// one obj of elements of bj1 nd obj2
+
+// console.log(Object.keys(objname));
+// console.log(Object.values(objname));
+// console.log(Object.entries(objname));       Array of key-value pair array
+
+console.log(objname.hasOwnProperty('email'));       //check property exist or nt=ot
+
+// value can also be fetch without dot
+// const {email:id}=user;       -->distructuring
+// console.log(id);
+
+
+
+
+ 
